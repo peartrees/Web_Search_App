72afe13e2adfbad5920e5103b81f37ab0ca9ebf8
peartrees <eggsandwich.petr@gmail.com>
2021-11-12 02:43:54 +0900

    search_engine_completed

--- backend/api_before.py
+++ backend/api_before.py
@@ -2,13 +2,11 @@ from flask import Blueprint, jsonify, request, session
 from flask_restful import Api, Resource
 import requests
 import json
-from googleapiclient.discovery import build
 
 # postされたテキストを検索するapi(POSTメソッド)
 Search_bp = Blueprint('Call_Search_API', __name__, url_prefix='/api/post')
 class Call_Search(Resource):
     def post(self):
-        total_result = []
         # postされたデータを読み込み
         input_data = request.json
         # Bing Web Search APIを利用してWeb検索結果を取得する
@@ -18,19 +16,9 @@ class Call_Search(Resource):
         # HTTPリクエストのヘッダにAPIキーを含める
         headers = {"Ocp-Apim-Subscription-Key" : SUBSCRIPTION_KEY}
         # 実際にリクエストを送る
-        bing_response = requests.get(search_url, headers=headers, params=params)
-        bing_result = bing_response.json()["webPages"]["value"]
-        total_result.append(bing_result)
-        # ↑↑↑↑ここまでがBingの検索↑↑↑↑
-        # ↓↓↓↓ここからGoogle検索↓↓↓↓
-        CUSTOM_SEARCH_ENGINE_ID = "4c81e5f5bf3c54cf3"
-        GOOGLE_API_KEY          = "AIzaSyDhwvVQkJsxQAzEyjQoyw2kWChfjB1YKJc"
-        search = build("customsearch","v1",developerKey = GOOGLE_API_KEY)
-        ggl_response = search.cse().list(q = input_data, cx = CUSTOM_SEARCH_ENGINE_ID,
-        lr = 'lang_ja',num = 9,start = 1).execute()
-        ggl_result = ggl_response["items"]
-        total_result.append(ggl_result)
-        return total_result
+        response = requests.get(search_url, headers=headers, params=params)
+        result = response.json()["webPages"]["value"]
+        return result
 
 Call_Search_API = Api(Search_bp)
 Call_Search_API.add_resource(Call_Search, '')
--- frontend/src/components/Search_before.vue
+++ frontend/src/components/Search_before.vue
@@ -2,30 +2,22 @@
   <v-row>
     <v-col cols=6>
       <v-card raised color="blue lighten-4">
-      <v-subheader><h3>Bing results</h3></v-subheader>
+      <v-subheader><h3>Google result</h3></v-subheader>
       <v-divider class="mx-3"></v-divider>
-      <v-list>
-        <v-list-item v-for="(value, index) in search_data[0]" :key="value.id">
-          <v-list-item-content>
-            <v-list-item-title>
-              <a v-bind:href="value.url" target="_blank">{{index+1}}. {{value.name}}</a>
-            </v-list-item-title>
-          <p v-html="value.snippet"></p>
-        </v-list-item-content>
-        </v-list-item>
-      </v-list>
+        <!-- <div class="body-1 mb-1">Displays Google Results</div> -->
+        <!-- <div class="body-2 mb-2">{{search_data[0]}}</div> -->
      </v-card>
     </v-col>
     <v-col cols=6>
       <v-card raised color="blue lighten-4">
-      <v-subheader><h3>Google results</h3></v-subheader>
+      <v-subheader><h3>Bing results</h3></v-subheader>
       <v-divider class="mx-3"></v-divider>
         <!-- <div class="body-1 mb-1">Displays Bing Results</div> -->
         <v-list>
-          <v-list-item v-for="(value, index) in search_data[1]" :key="value.cacheid">
+          <v-list-item v-for="(value, index) in search_data" :key="value.id">
             <v-list-item-content>
               <v-list-item-title>
-                <a v-bind:href="value.link" target="_blank">{{index+1}}. {{value.title}}</a>
+                <a v-bind:href="value.url" target="_blank">{{index+1}}. {{value.name}}</a>
               </v-list-item-title>
             <p v-html="value.snippet"></p>
           </v-list-item-content>
